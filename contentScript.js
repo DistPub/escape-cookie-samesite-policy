555dd40f9006d98abd89ddd3a219cce7f9b40a35
Smite Chow <xiaopengyou2no1@gmail.com>
2021-08-13 15:08:06 +0800

    need user confirm add whitelist

--- contentScript.js
+++ contentScript.js
@@ -4,6 +4,12 @@ window.addEventListener('message', event=>{
   }
 
   if (event.data.destination === 'edge-lover-bridge') {
+    if (event.data.payload.method === 'add-whitelist') {
+      if(!confirm(`Allow this website read and change your data on ${event.data.payload.data}?`)) {
+        return window.postMessage({source: 'edge-lover-bridge', payload: null, id: event.data.id});
+      }
+    }
+
     chrome.runtime.sendMessage(event.data.payload, response => {
       window.postMessage({source: 'edge-lover-bridge', payload: response, id: event.data.id});
     });
